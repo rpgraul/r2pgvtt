d4f1326682dd45a9d2ce39f153dc5177d7d3e7d7
rpgraul <rpgraul@hotmail.com>
2026-04-17 19:24:26 -0300

    chore: remove unused build artifacts and update SvelteKit output configuration

--- public/.svelte-kit/output/server/nodes/1.js
+++ public/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.oI1iwH0-.js","_app/immutable/chunks/wRaPAkex.js","_app/immutable/chunks/BesqfEI8.js","_app/immutable/chunks/sV_nQZLa.js","_app/immutable/chunks/CnkBgILj.js","_app/immutable/chunks/B7aRKOZ2.js","_app/immutable/chunks/DiGqoWnr.js"];
+export const imports = ["_app/immutable/nodes/1.CfwQ3giG.js","_app/immutable/chunks/wRaPAkex.js","_app/immutable/chunks/BesqfEI8.js","_app/immutable/chunks/sV_nQZLa.js","_app/immutable/chunks/CnkBgILj.js","_app/immutable/chunks/Cn4A6blf.js","_app/immutable/chunks/DiGqoWnr.js"];
 export const stylesheets = [];
 export const fonts = [];
--- public/dist/_app/immutable/nodes/0.Df6Xs8Xk.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,a as n}from"../chunks/C3_6il5z.js";export{e as component,n as universal};
